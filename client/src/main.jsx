7790fd08b576ec512555631fe84f4a4b5930922a
Volodymyr Shpachuk <vladimir.shpachuk@gmail.com>
2023-04-01 20:22:02 +0300

    2: Update folder tree

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/screens/home/Home.jsx'
+import App from './components/home/Home.jsx'
 import './assets/styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
